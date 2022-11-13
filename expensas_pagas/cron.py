bd4cb17d5df2d8fcc4b1828f54e4fa7af2c0ae89
Agustin <ramosagustin2103@gmail.com>
2022-11-13 10:26:26 -0300

    first commit

---
+++ expensas_pagas/cron.py
@@ -0,0 +1,10 @@
+from .manager import *
+
+def procesar_expenas_pagas():
+    #e = Exp()
+    #e.exp_import()
+    #e.exp_export()
+
+    #e = NewExp()
+    #e.request_inf_deuda()
+    NewExp().cobros_exp()
